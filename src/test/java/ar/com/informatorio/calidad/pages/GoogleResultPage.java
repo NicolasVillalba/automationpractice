72cbf3250e020ada1b6975ae49ee605e32a4bc3f
Nicolas Villalba <nerynicolas@gmail.com>
2018-08-30 01:09:07 -0300

    Refactored version that extend from BasePage class now

--- src/test/java/ar/com/informatorio/calidad/pages/GoogleResultPage.java
+++ src/test/java/ar/com/informatorio/calidad/pages/GoogleResultPage.java
@@ -5,20 +5,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class GoogleResultPage {
-
-	private WebDriver driver;
+public class GoogleResultPage extends BasePage{
 
 	@FindBy(xpath = "(//*[@class='g']/descendant::a)[1]")
 	private WebElement firstResultLink;
-
+	
 	public GoogleResultPage(WebDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
+		super(driver);
 	}
-
+	
+	/**
+	 * for opening a new page 
+	 * @param returnPage any class that receives as argument
+	 * @return an instance of the new page Object created with Page factory
+	 */
 	public <T> T openFirstResult(Class<T> returnPage) {
 		firstResultLink.click();
-		return PageFactory.initElements(driver, returnPage);
+		return PageFactory.initElements(this.driver, returnPage);
 	}
 }
