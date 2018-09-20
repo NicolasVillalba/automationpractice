88be808037d6b4657e3086943560ce4f83e842c5
Nicolas Villalba <nerynicolas@gmail.com>
2018-09-20 03:56:21 -0300

    this commit was done because i forgot to save the file until to do it

--- src/test/java/ar/com/informatorio/calidad/pages/GoogleResultPage.java
+++ src/test/java/ar/com/informatorio/calidad/pages/GoogleResultPage.java
@@ -5,7 +5,6 @@ import java.lang.reflect.InvocationTargetException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 import ar.com.informatorio.calidad.Exceptions.ReflectionConstrutorInvocationException;
 
