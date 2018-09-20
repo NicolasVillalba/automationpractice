637c9c9ebd6c29a090b99c120dac27dfa2871c9c
Nicolas Villalba <nerynicolas@gmail.com>
2018-09-20 03:50:24 -0300

    refactored openFirstResult() to get rid of PageFactory

--- src/test/java/ar/com/informatorio/calidad/pages/GoogleResultPage.java
+++ src/test/java/ar/com/informatorio/calidad/pages/GoogleResultPage.java
@@ -1,9 +1,13 @@
 package ar.com.informatorio.calidad.pages;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import ar.com.informatorio.calidad.Exceptions.ReflectionConstrutorInvocationException;
 
 public class GoogleResultPage extends BasePage{
 
@@ -18,9 +22,22 @@ public class GoogleResultPage extends BasePage{
 	 * for opening a new page 
 	 * @param returnPage any class that receives as argument
 	 * @return an instance of the new page Object created with Page factory
+	 * @throws Exception 
+	 * @throws SecurityException 
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalArgumentException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public <T> T openFirstResult(Class<T> returnPage) {
+	
+	public <T extends BasePage> T openFirstResult(Class<T> returnPage) throws ReflectionConstrutorInvocationException {
 		firstResultLink.click();
-		return PageFactory.initElements(this.driver, returnPage);
+		try {
+			return returnPage.getConstructor(WebDriver.class).newInstance(this.driver);
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new ReflectionConstrutorInvocationException("The strategy for Object Construction might fail");
+		}
 	}
 }
