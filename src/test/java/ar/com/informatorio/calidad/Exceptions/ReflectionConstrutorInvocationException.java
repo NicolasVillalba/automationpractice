0e5dd87d836469ef26261ae5b240be2d43418a5a
Nicolas Villalba <nerynicolas@gmail.com>
2018-09-25 08:03:31 -0300

    added ReflectionContructorInvocationException v1

---
+++ src/test/java/ar/com/informatorio/calidad/Exceptions/ReflectionConstrutorInvocationException.java
@@ -0,0 +1,12 @@
+package ar.com.informatorio.calidad.Exceptions;
+
+public class ReflectionConstrutorInvocationException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ReflectionConstrutorInvocationException(String msg) {
+		super(msg);
+	}
+}
