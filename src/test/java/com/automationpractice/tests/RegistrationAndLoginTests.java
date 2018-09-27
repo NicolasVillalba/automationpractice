a858bb7d22b67a572560ff456f1e4cb4abec3f2c
Nicolas Villalba <nerynicolas@gmail.com>
2018-09-27 12:03:00 -0300

    added sing in test, beforeMethod to setup URL, updated the changes in HomePage PO

--- src/test/java/com/automationpractice/tests/RegistrationAndLoginTests.java
+++ src/test/java/com/automationpractice/tests/RegistrationAndLoginTests.java
@@ -5,32 +5,38 @@ import static org.testng.Assert.assertTrue;
 import java.util.StringJoiner;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.automationpractice.pages.AuthenticationPage;
 import com.automationpractice.pages.CreateAnAccountPage;
 import com.automationpractice.pages.HomePage;
+import com.automationpractice.pages.SingInPage;
 import com.automationpractice.pages.CreateAnAccountPage.CUSTOMER_TITLE;
 
+import ar.com.informatorio.calidad.Exceptions.ReflectionConstrutorInvocationException;
 import ar.com.informatorio.calidad.tests.BaseTest;
 
 public class RegistrationAndLoginTests extends BaseTest {
 
 	private String email;
 
-	@Test
-	public void firstStep() throws InterruptedException {
-
+	@BeforeMethod
+	public void setUp() {
 		this.driver.get("http://automationpractice.com/index.php");
+	}
+	
+	@Test(enabled = false, testName = "Registration Test")
+	public void firstStep() throws InterruptedException, ReflectionConstrutorInvocationException {
 		
 		StringJoiner joinstring = new StringJoiner("_");
 		this.email = joinstring.add(RandomStringUtils.random(4, true, false)).add("jhondoe@info.net").toString();
 
 		HomePage homepage = new HomePage(this.driver);
 
-		AuthenticationPage authpage = homepage.clickSingIn();
+		AuthenticationPage authpage = homepage.clickSingIn(AuthenticationPage.class);
 
-		// janedoe & jhondoe was used
+		// janedoe@info.ner & jhondoe was used
 		CreateAnAccountPage createAcountform = authpage.insertEmailAndPress(email);
 
 		assertTrue(createAcountform.checkTitle("CREATE AN ACCOUNT"));
@@ -53,7 +59,17 @@ public class RegistrationAndLoginTests extends BaseTest {
 				.enterMobilePhone(98434893)
 				.enterAddressAlias("")
 				.clickRegister();
-
+		
 		assertTrue(createAcountform.checkTitle("MY ACCOUNT"));
+	}
+	
+	@Test(enabled = true)
+	public void loginTest() throws ReflectionConstrutorInvocationException {
+		HomePage homePage = new HomePage(this.driver);
+		SingInPage singInPage = homePage.clickSingIn(SingInPage.class);
+		singInPage.enterEmail("janedoe@info.ner");
+		singInPage.enterPassword("very-hard-to-guess-passw");
+		singInPage.clickSingInButton();
+		assertTrue(singInPage.customerNameIsPresent("Donald Trump"));
 	}
 }
