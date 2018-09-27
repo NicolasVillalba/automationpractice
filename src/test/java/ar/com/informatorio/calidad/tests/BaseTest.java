0a1cebb39f90ad74c95c19147920d6083b9f5d09
Nicolas Villalba <nerynicolas@gmail.com>
2018-09-27 12:05:15 -0300

    added Google Guice to Inject the Web Driver, beforeMethod was removed

--- src/test/java/ar/com/informatorio/calidad/tests/BaseTest.java
+++ src/test/java/ar/com/informatorio/calidad/tests/BaseTest.java
@@ -1,25 +1,21 @@
 package ar.com.informatorio.calidad.tests;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Guice;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
+import com.google.inject.Inject;
 
+import ar.com.informatorio.calidad.dependencies.DriverModule;
+
+@Guice(modules = { DriverModule.class })
 public class BaseTest {
 	
+	@Inject
 	public WebDriver driver;
-
-	@BeforeClass
-	public void setUp() {
-		ChromeDriverManager.getInstance().setup();
-		driver = new ChromeDriver();
-	}
-	
 
 	@AfterClass
 	public void teardown() {
-		//driver.quit();
+		driver.quit();
 	}
 }
