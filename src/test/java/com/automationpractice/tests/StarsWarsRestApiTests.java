0a68b04e4ad03c179d66a7b0872b9877b16fc2ed
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-17 06:26:03 -0300

    black test

---
+++ src/test/java/com/automationpractice/tests/StarsWarsRestApiTests.java
@@ -0,0 +1,16 @@
+package com.automationpractice.tests;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class StarsWarsRestApiTests {
+	
+	@Test
+	public void testRestAsssure() {
+		
+	}
+
+}
